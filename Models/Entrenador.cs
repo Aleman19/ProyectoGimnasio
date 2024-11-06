8eebf28f920a3d571fae0b3f3d0d354496b4eebb
Jonmb026 <jonathanmb026@gmail.com>
2024-11-05 20:49:41 -0600

    Creación de la base de las carpetas

---
+++ Models/Entrenador.cs
@@ -0,0 +1,9 @@
+namespace ProyectoGimnasio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
